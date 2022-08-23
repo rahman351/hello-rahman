f7e89f0157615c3ad7561fd4ae6afdc228e32fa0
rahman351 <itzemerahmansyed@gmail.com>
2022-08-23 18:18:09 +0530

    Added alt for images

--- src/RightContainer/Projects/Projects.js
+++ src/RightContainer/Projects/Projects.js
@@ -9,20 +9,20 @@ const Projects = () => {
             <div className="project-bottom-container">
                 <div className="single-project-container">
                     <div className="single-project-left-container">
-                        <img src="/calculator.png" width="300px"/>
+                        <img src="/calculator.png" width="300px" alt="calculator"/>
                     </div>
                     <div className="single-project-right-container">
                         <h2> Loan EMI Calculator</h2>
                         <p> A Modern Loan EMI Calculator made from scratch with ReactJs and Charts Library</p>
                         <p> 
                             <button className='git-btn' onClick={()=> {window.open("https://github.com/rahman351/loan-emi-calc")}}>Source Code</button>
-                            <button className='demo-btn' onClick={()=> {window.open("https://loan-emi-calc.netlify.app/")}}>Live Demo</button>
+                            <button className='demo-btn' onClick={()=> {window.open("https://loan-emi-calc.000webhostapp.com/")}}>Live Demo</button>
                         </p>
                     </div>
                 </div>
                 <div className="single-project-container">
                     <div className="single-project-left-container">
-                    <img src="/notebook.png" width="300px"/>
+                    <img src="/notebook.png" width="300px" alt="notebook"/>
                     </div>
                     <div className="single-project-right-container">
                         <h2> Note Taking app</h2>
