e324b00239ff4a101d6dd9703ab5c41260357132
rahman351 <itzemerahmansyed@gmail.com>
2022-08-23 20:12:21 +0530

    Added age info

--- src/RightContainer/About/About.js
+++ src/RightContainer/About/About.js
@@ -1,9 +1,11 @@
 import './About.css';
 const AboutPage = () => {
+    const age = (new Date().getFullYear())-2001;
     return (
         <div className="about-container">
             <h1>Hi,<br/> I am Syed Rahmanudeen </h1>
-            <h2>young self taught web developer</h2>
+            <h2>{age} years old </h2>
+            <h2>Passionate self taught web developer</h2>
             <div className="btn-container">
                 <button className='about-btn' onClick={()=>window.location.href="/skills"}>Check out my skills</button>
                 <button className='about-btn' onClick={()=>window.location.href="/projects"}>Check out my web development projects</button>
